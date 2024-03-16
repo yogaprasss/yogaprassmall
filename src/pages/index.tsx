6a497372e3cbbb7969ac67ac5cb93f2c0068fad0
yoga <prasetyo.yoga1109@gmail.com>
2024-03-17 00:01:39 +0700

    adding server side rendering on detail product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import HomeView from '@/views/Home';
 
 import { NextSeo } from 'next-seo';
@@ -5,10 +6,16 @@ import { NextSeo } from 'next-seo';
 const Home = () => {
   return (
     <>
-      <NextSeo
+      {/* <NextSeo
         title='yogaprassmall'
         description='Boleh, cari apa kakak?'
-      />
+      /> */}
+      <Head>
+        <title>yogaprassmall</title>
+        <meta name='description' content='Boleh, cari apa kakak?' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='og:image' content='https://yogaprassmall.vercel.app/opengraph-image.jpg' />
+      </Head>
       <HomeView />
     </>
   );
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,31 @@
+import Head from 'next/head';
 import ProductView from '@/views/Product';
 
-const Product = () => {
+import type { ProductProps } from '@/utils/types';
+import type { NextPageContext } from 'next';
+
+const Product = ({ title, images }: ProductProps) => {
   return (
-    <ProductView />
+    <>
+      <Head>
+        <title>{title} | yogaprassmall</title>
+        <meta name='description' content='Belanja murah yuk kakak!' />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='og:image' content={images[0]} />
+      </Head>
+      <ProductView />
+    </>
   );
+};
+
+export async function getServerSideProps({ query }: NextPageContext) {
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  const slug = `/products/${query.id as string}`;
+
+  const result = await fetch(baseURL + slug);
+  const data = await result.json() as ProductProps;
+
+  return { props: { ...data } };
 };
 
 export default Product;
